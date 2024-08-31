0113b8673579899efcfa63c74a2a073ee6a8228b
Hussein Gaber <148823784+hussaingaber@users.noreply.github.com>
2024-08-31 21:53:09 +0300

    Add missing methods

--- src/Contracts/PaymentGatewayInterface.php
+++ src/Contracts/PaymentGatewayInterface.php
@@ -11,4 +11,11 @@ interface PaymentGatewayInterface
     public function createPaymentPage(Payment $details): array;
 
     public function queryTransaction(string $transactionReference): array;
+
+    public function capturePayment(string $transactionReference, float $amount, string $currency, string $cartId, string $cartDescription): array;
+
+    public function refundPayment(string $transactionReference, float $amount, string $currency, string $cartId, string $cartDescription): array;
+
+    public function voidPayment(string $transactionReference, float $amount, string $currency, string $cartId, string $cartDescription): array;
+
 }
